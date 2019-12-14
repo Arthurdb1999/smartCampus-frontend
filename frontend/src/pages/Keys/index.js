6c0c47c4fcc2007f39283b82343a26c21106a506
arthurdb1999 <arthurdb1999@gmail.com>
2019-12-14 01:08:55 -0300

    Falta integrar um serviço

--- frontend/src/pages/Keys/index.js
+++ frontend/src/pages/Keys/index.js
@@ -5,16 +5,29 @@ import api3 from '../../services/api3';
 export default function Keys() {
 
     const [keys, setKeys] = useState([]);
+    const [reserves, setReserves] = useState([]);
 
     useEffect(() => {
         async function handleKeys() {
             const response = await api3.get('/chaves');
+            const response2 = await api3.get('/retiradas');
             setKeys(response.data);
+            setReserves(response2.data);
         }
 
         handleKeys();
 
-    }, [keys]);
+        console.log(keys);
+        console.log(reserves);
+
+    }, []);
+
+    // function filterByKey(id){
+    //     console.log(id)
+    //     return reserves.filter(reserve => {
+    //         return reserve.indexOf(id);
+    //     })
+    // }
    
     return (
         <>
@@ -26,6 +39,8 @@ export default function Keys() {
                             <strong>Sala  {key.sala}</strong>
                             <br/>
                             <strong>Descrição:  </strong>  {key.descricao}
+                            <br/>               
+                            {/* <strong>xablau:  </strong> {filterByKey(key.id)} */}
                         </span>
                     </li>
                 ))}
